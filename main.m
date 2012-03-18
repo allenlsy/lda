cd8231962dbdd960f1aa6f5aaf30fd7e938ede5b
allenlsy <cafe@allenlsy.com>
2012-03-18 11:51:30 +0400

    ImageDB -> FaceDB. dimensionality reduced.

--- main.m
+++ main.m
@@ -1,16 +1,25 @@
+
+
+
+
+
 % 0. Initialization
 pkg load image;
 pkg load strings;
 
 
 % 1. Face Image Cropping and Preprocessing
-
+rs = 3;
+cs = 3;
 
 
 % 2. Construct ImageDB: Read Images
+
+disp('Constructing imageDb...')
 if ( ~exist('imageDb.data', "file") )  % if not exists imageDb file
+    disp('  no imageDb.data. Now creating imageDb');
     cd dataset;
-    folder = dir('.')
+    folder = dir('.');
     for i=1:40
         folderName = folder(i+3).name;
         subdir = dir( folderName );
@@ -19,38 +28,69 @@ if ( ~exist('imageDb.data', "file") )  % if not exists imageDb file
             imageDb(index).label = i;
             img = imread( strcat( folderName, '/', subdir(j+2).name ) );
             s = size(img);
-            imageDb(index).image = reshape( img, s(1)*s(2), 1) ;
+            
+            % decrease img
+            [r,c] = size(img);
+            temp=[];
+            k=1;
+            while k<=r,
+                temp = [temp;img(k,:)];
+                k+=rs;
+            end
+            k=1;
+            res=[];
+            while k<=c,
+                res=[res, temp(:,k)];
+                k+=cs;
+            end
+           
+            s = size(res);
+            imageDb(index).image = reshape( double(res), s(1)*s(2), 1) ;
         end
 
     end
+    D=s(1)*s(2);
     cd ..;
     save -binary imageDb.data imageDb
+else
+    load("-binary", "imageDb.data", "imageDb");
+    s=size(imageDb(1).image);
+    D=s(1)*s(2); % original size
 end
 
-load("-binary", "imageDb.data", "imageDb");
-
-D=s(1)*s(2); % original size
-
+D
 % 3. Construct FeatureDB: Extract Features
 % imageDb;
 
 % 4. Dimensionality Reduction: PCA
+disp('Dimensinality Reduction...')
 
 % calculate xavg, X
-n = length(imageDb);
+disp('  calculate xavg, X');
+n = length(imageDb)
+xavg = double(zeros(D, 1));
+X=double([]);
+
 for i=1:n
-    xavg += imageDb(1).image;
+    xavg += double(imageDb(i).image);
+end
+for i=1:D
+    xavg(i) = xavg(i)/n;
 end
 for i=1:n
     X=[X imageDb(i).image-xavg];
 end
-X
-
+disp('X size');
+size(X)
 % calculate C, Sigma
-C = 1/n*X*transpose(X)
-[P, Sigma] = Eig(C)
+
+disp('  calculate C, Sigma');
+C = (1/n)*X*transpose(X);
+[P, Sigma] = eig(C);
+Sigma=diag(Sigma);
 
 % After achieving P and Sigma, run all possible d and report the best results, based on energy criterion
+disp('  calculating d');
 sum = zeros(1, length(Sigma) );
 for i=1:length( Sigma )
     sum += Sigma(i);
@@ -58,19 +98,26 @@ end
 
 temp = zeros(1, length(Sigma) );
 max = 0;
-for i=1:length( Sigma )
+for i=1:length( Sigma ),
     temp += Sigma(i);
     if temp/sum > max,
         max = temp/sum;
         d = i;
     end
 end
+d
+PCAMtx = transpose(P(:,[1:d])); 
 
 % 5. Construct FaceDB: Project Data to low-dimensional space
-
+disp('Construct FaceDB');
 dbSize = length(imageDb);
-for i=1:dbSize
-    
-  
+size(imageDb(1).image)
+
+for i=1:dbSize,
+    faceDb(i).label = imageDb(i).label;
+    faceDb(i).image = PCAMtx * imageDb(i).image;
+end
+length(faceDb)
+size(faceDb(1).image)
 
 % 6. Classification: Nearest Neighbor
